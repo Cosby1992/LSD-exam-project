64c54f939c7a9eeb0fea40d7e7cf799d7475b56a
cosby1992 <andersbirkedahl@hotmail.com>
2021-12-08 12:56:46 +0100

    test passed with current implementation code-generator should produce string of provided length

--- user-api/tests/unit_tests/code-genrator.test.js
+++ user-api/tests/unit_tests/code-genrator.test.js
@@ -7,9 +7,11 @@ test('code-generator should produce string of length 4', () => {
 })
 
 test('code-generator should generate string with only specified characters', () => {
-
     for (const char of generateCode()) {
         expect(characters.includes(char)).toStrictEqual(true);
     }
-    
+})
+
+test('code-generator should produce string of provided length', () => {
+    expect(generateCode(10).length).toStrictEqual(10);
 })
