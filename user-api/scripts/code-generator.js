db440463db55bb2ea790c9b2f375c9cf6acb0663
cosby1992 <andersbirkedahl@hotmail.com>
2021-12-08 12:49:29 +0100

    Test implemented for testing if code includes correct characters

--- user-api/scripts/code-generator.js
+++ user-api/scripts/code-generator.js
@@ -1,11 +1,11 @@
 
 
-// const codeCharacters = [
-//     'a','b','c','d','f','g','h','i','j','k',
-//     'l','m','n','o','p','q','r','s','t','u',
-//     'v','w','x','y','z','0','1','2','3','4',
-//     '5','6','7','8','9' 
-// ];
+const codeCharacters = [
+    'a','b','c','d','f','g','h','i','j','k',
+    'l','m','n','o','p','q','r','s','t','u',
+    'v','w','x','y','z','0','1','2','3','4',
+    '5','6','7','8','9' 
+];
 
 exports.generateCode = function(length = 4) {
 
--- user-api/tests/unit_tests/code-genrator.test.js
+++ user-api/tests/unit_tests/code-genrator.test.js
@@ -1,6 +1,15 @@
 const {generateCode} = require('../../scripts/code-generator');
+const characters = require('../../config').codegeneration.characters;
 
 
 test('code-generator should produce string of length 4', () => {
     expect(generateCode().length).toStrictEqual(4)
+})
+
+test('code-generator should generate string with only specified characters', () => {
+
+    for (const char of generateCode()) {
+        expect(characters.includes(char)).toStrictEqual(true);
+    }
+    
 })
