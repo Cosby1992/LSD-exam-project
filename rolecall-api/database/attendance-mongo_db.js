badd95a063a8a7e422b7f89ddf45238992949642
cosby1992 <andersbirkedahl@hotmail.com>
2021-12-20 12:41:02 +0100

    comments added

--- rolecall-api/database/attendance-mongo_db.js
+++ rolecall-api/database/attendance-mongo_db.js
@@ -6,17 +6,20 @@ const url = "mongodb://" + config.mongodb.host + ":" + config.mongodb.port;
 const client = new MongoClient(url);
 const dbName = config.mongodb.fakecphdbname;
 
+// Create important indexes if they do not exist
 const createIndexes = async () => {
   await client.connect();
   const db = client.db(dbName);
   const collection = db.collection("attendance");
 
+  // Set time to live on the documents
   try {
     await collection.createIndex({ expires_at: 1 }, { expireAfterSeconds: 0 });
   } catch (error) {
     console.log(error);
   }
 
+  // Create index on lecture_id, since this is used to query the database
   try {
     await collection.createIndex({ lecture_id: 1 });
   } catch (error) {
@@ -26,8 +29,9 @@ const createIndexes = async () => {
 
 createIndexes();
 
-// insert an attendance document
+// Insert an attendance document
 exports.publishAttendanceCodeWithTTL = async function (lecture_id, teacher_id, code, ttl = 15) {
+  // Check params
   if (
     typeof lecture_id !== "string" ||
     typeof code !== "string" ||
@@ -45,13 +49,16 @@ exports.publishAttendanceCodeWithTTL = async function (lecture_id, teacher_id, c
   // // Check if lecture exists with teacher id
   // await find('lectures', {_id: ObjectId(lecture_id), teacher_docref: teacher_id,  end: {$gte: new Date()}})
   
+  // Connect to the database
   await client.connect();
   const db = client.db(dbName);
   const collection = db.collection("attendance");
 
+  // Create the expiration date using time to live
   const expire_date = new Date();
   expire_date.setSeconds(expire_date.getSeconds() + ttl);
 
+  // Insert the document and return the result from the db
   return await collection.insertOne({
     lecture_id: lecture_id,
     attendance_code: code,
@@ -59,46 +66,60 @@ exports.publishAttendanceCodeWithTTL = async function (lecture_id, teacher_id, c
   });
 };
 
+// Check if a code exists with the lecture id in the attendance db
 exports.checkIfCodeMatches = async (code, lecture_id) => {
+  // Check params
   if (typeof lecture_id !== "string" || typeof code !== "string")
     throw new Error("lecture_id and code must be strings");
 
+  // Connect to the database
   await client.connect();
   const db = client.db(dbName);
   const collection = db.collection("attendance");
 
+  // Query for lecture
   let attendanceDoc = collection.find({lecture_id: lecture_id});
 
+  // If it does not exist
   if (!await attendanceDoc.hasNext()) {
     throw new Error("The Lecture does not exist");
   }
 
+  // If the lecture exist, check if the code matches
+  // OBS multiple codes can be valid at the same time
   while (await attendanceDoc.hasNext()) {
     let currentDoc = await attendanceDoc.next();
     if(currentDoc?.attendance_code === code) {
+      // Return true if the code matches one of the found lectures
       return true;
     }
   }
 
+  // If the code does not match
   return false;
 };
 
+// Method to query the db
 async function find(collection_name, query) {
 
+  // Connect 
   await client.connect();
   const db = client.db(dbName);
   const collection = db.collection(collection_name);
   let result = collection.find(query);
   
+  // If nothing is found
   if(!await result.hasNext()) {
       throw new Error('Not found: Nothing found with the given arguments');
   }
 
+  // Add what is found to array
   let lectures = [];
   while(await result.hasNext()) {
       lectures.push(await result.next())
   }
 
+  // Return results as array
   return lectures;
 }
 
--- rolecall-api/routes/lecture.js
+++ rolecall-api/routes/lecture.js
@@ -7,11 +7,12 @@ const { publishAttendanceCodeWithTTL } = require('../database/attendance-mongo_d
 const { generateCode } = require('../scripts/code-generator');
 const {min_ttl, max_ttl} = require('../config').codegeneration;
 
-/* GET home page. */
+/* POST to start a lecture. */
 router.post("/start", async function (req, res, next) {
 
   const header = req.headers.authorization;
 
+  // Check auth
   if (!header?.includes("JWT:"))
     sendResponse(
       res,
@@ -26,21 +27,27 @@ router.post("/start", async function (req, res, next) {
     sendResponse(res,403,"Forbidden","You are not authorized to use this endpoint, please login first.");
 }
 
+  // Check body params
   if(!req.body.lecture_id) {
     sendResponse(res, 400, "Bad Request", "lecture_id was not found");
   }
 
   try {
+    // Read the data in the token
     getTokenData(token, async (payload, header) => {
 
+      // if the user is not a teacher
       if(payload.role !== 'TEACHER') {
         sendResponse(res, 403, "Forbidden", "You are not authorized to use this endpoint, only teachers can start lectures");
       }
       
+      // Generate the attendance code 
       const generatedCode = generateCode();
 
       let updateResult;
 
+      // Publish the code with the lecture, making it possible to check in for a student
+      // TTL is how long the code will be available for
       try {
         if(req.body?.ttl && typeof req.body.ttl === 'number' && req.body.ttl > min_ttl && req.body.ttl < max_ttl) {
           updateResult = await publishAttendanceCodeWithTTL(req.body.lecture_id, payload.user_id, generatedCode, req.body.ttl);
@@ -59,6 +66,7 @@ router.post("/start", async function (req, res, next) {
         return;
       }
 
+      // Problem reaching the db
       if(!updateResult?.acknowledged) {
         console.log("Update result: ");
         console.log(updateResult);
@@ -71,6 +79,8 @@ router.post("/start", async function (req, res, next) {
         return;
       }
       
+      // If all goes well, 
+      // return the code
       res.send({
         status: 200,
         message: "OK",
@@ -79,6 +89,7 @@ router.post("/start", async function (req, res, next) {
 
     });
   } catch (err) {
+    // Token not valid
     sendResponse(
       res,
       403,
@@ -89,6 +100,7 @@ router.post("/start", async function (req, res, next) {
   }
 });
 
+// Method to send a response
 const sendResponse = (res, status, statusDescribtion, message) => {
   res.status(status).send({
     status: status + ": " + statusDescribtion,
